8fba8dca248bc1d1077a117e7485a76ed73a9203
Bristovic <103121649+Bristovic@users.noreply.github.com>
2024-02-08 12:39:05 +0100

    Changed file structure

---
+++ Ch4/4_1_DataInDataOut.R
@@ -0,0 +1,11 @@
+# reading and writing data
+
+# reading and writing from CSV
+ex_data <- read_csv("PATH/FILENAME_RAW_DATA.csv")
+write_csv(processed_data, "PATH/FILENAME_PROCESSED_DATA.csv")
+
+# reading or altering delimiters between cells
+data_from_strange_file <- read_delim("WEIRD_DATA_FILE.txt", delim = "|")
+write_delim(data_string, "PATH/FILENAME_DATA_STRING.txt", delim = "|")
+
+
